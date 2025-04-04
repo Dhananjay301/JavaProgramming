9430b4a1e4dc877229bd318035c0dbf7b7fbeb65
Dhananjay301 <dhananjaypatil301@gmail.com>
2025-04-04 12:35:30 +0530

    Second commit

--- src/day6/SingleDimesionalAray.java
+++ src/day6/SingleDimesionalAray.java
@@ -5,26 +5,26 @@ public class SingleDimesionalAray {
 	public static void main(String[] args) {
 
 //		Declare array and Assign vale
-		int a[]=new int[3];
-		a[0]=1;
-		a[1]=2;
-		a[2]=3;
-		
+		int a[] = new int[3];
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 3;
+
 //		read single value
 		System.out.println("Index at 1st position is = " + a[1]);
-		
+
 //		size of array
 		System.out.println("size of array = " + a.length);
-		
+
 //		read multiple value
 		for (int x : a) {
 			System.out.println(x);
-			
-//			approach second- normal for loop
-			for (int i=0; i<=a.length-1;i++) {
-				System.out.println("Element at " + i + " ="+a[i]);
-			}
 		}
+//			approach second- normal for loop
+		for (int i = 0; i <= a.length - 1; i++) {
+			System.out.println("Element at " + i + " =" + a[i]);
+		}
+
 	}
 
 }
